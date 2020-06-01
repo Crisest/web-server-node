e6ac1c7eaa6ca55c660c9964ac5671b3bf4b1e16
Yorguin Murillo <ymurillo2@ncstudents.niagaracollege.ca>
2020-06-01 01:14:01 -0400

    Redirect to portfolio

--- src/app.js
+++ src/app.js
@@ -84,7 +84,7 @@ app.get('/help', (req, res)=>{
 app.get('/help/*', (req, res) =>{
     res.render('404', {
         title: '404',
-        name: 'Yorguin Muyrillo',
+        name: 'Yorguin Murillo',
         errorMessage: 'Page article not found'
     })
 })
